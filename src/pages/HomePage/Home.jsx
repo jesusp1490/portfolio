369bf63a77e8a2fc5ab56e7fbbd27dd458859742
Jesus <jesusperez_14@hotmail.com>
2023-12-18 12:06:13 +0100

    Home page styles

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Typed from "typed.js";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import Navbar from "../../components/Navbar/Navbar"
+import Navbar from "../../components/Navbar/Navbar";
 import './_Home.scss';
 
 const Home = () => {
@@ -12,6 +12,7 @@ const Home = () => {
             strings: ['Full Stack Developer', 'Video Games Developer', 'Photographer'],
             typeSpeed: 50,
             backSpeed: 30,
+            backDelay: 1000,
             loop: true,
         };
 
@@ -26,20 +27,29 @@ const Home = () => {
     return (
         <div className="home-container">
             <Navbar />
-            <img src="https://res.cloudinary.com/dizd9f3ky/image/upload/v1702648552/Praga_122_phegkz.jpg" alt="Profile_Prague" className="home-img"/>
-            <div className="home-name">Jesus Perez</div>
-            <div id="typed" className="home-professions"></div>
-            <div className="home-description"></div>
-            <div className="home-rrss">
-                <a href="https://github.com/jesusp1490" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGithub} className="home-rrss-icon" />
-                </a>
-                <a href="https://www.linkedin.com/in/jesusperez14/" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faLinkedin} className="home-rrss-icon" />
-                </a>
-                <a href="https://www.instagram.com/jesusperez.ph/" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faInstagram} className="home-rrss-icon" />
-                </a>
+            <div className="home-content">
+                <div className="home-avatar">
+                    <img src="https://res.cloudinary.com/dizd9f3ky/image/upload/v1702648552/Praga_122_phegkz.jpg" alt="Profile_Prague" className="home-img" />
+                </div>
+                <div className="home-details">
+                    <h3 className="home-name">Jesus Perez</h3>
+                    <h4 className="home-typed">I`m a <span id="typed"></span></h4>
+                    <p className="home-description">Passionate Full Stack Developer with a love for photography, 3D art, and a strong affinity for music and movies. I´m dedicated to
+                        crafting smooth and engaging digital experiences. I blend the art of design with coding expertise to turn creative ideas into
+                        live, user-friendly solutions. Let`s bring your visions to life in the digital world!
+                    </p>
+                    <div className="home-rrss">
+                        <a href="https://github.com/jesusp1490" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGithub} className="home-rrss-icon" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/jesusperez14/" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faLinkedin} className="home-rrss-icon" />
+                        </a>
+                        <a href="https://www.instagram.com/jesusperez.ph/" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faInstagram} className="home-rrss-icon" />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
